6249855b0af697f07c23ae44333e8b3bc6931d86
WolfgangPolska <wolfgangpolska@gmail.com>
2020-05-13 17:02:40 +0200

    Pierwsza połowa projektu
    Utworzono sam algorytm, dodano punkty wejścia i wyjścia.

---
+++ Genetic Algorithm - path finding/Assets/Scripts/Genompathfinder.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genompathfinder : MonoBehaviour
+{
+    #region Fields
+    public DNA dna;
+
+    public float creatureSpeed;
+
+    public float pathMultiplier;
+
+    int pathIndex = 0;
+
+    public bool hasFinished = false;
+
+    public bool hasInitialized = false;
+
+    Vector2 target;
+
+    Vector2 nextPoint;
+
+    public float fitness
+    {
+         get
+        {
+            float dist = Vector2.Distance(transform.position, target);
+            if(dist == 0f)
+            {
+                dist = 0.0001f;
+            }
+
+            return (60/ dist);
+        }
+    
+
+    }
+
+
+
+#endregion
+
+#region Metohds
+
+
+/*
+
+private void Start()
+{
+    initCreature(new DNA(), Vector2.zero);        
+}
+
+*/
+
+public void initCreature(DNA newDNA, Vector2 _target)
+    {
+        dna = newDNA;
+        target = _target;
+        nextPoint = transform.position;
+        hasInitialized = true;
+    }
+    
+
+    private void Update()
+    {
+        if(hasInitialized && !hasFinished)
+        {
+            if(pathIndex == dna.genes.Count || Vector2.Distance(transform.position,target) < 0.5f)
+            {
+                hasFinished = true;
+            }
+
+            if ((Vector2)transform.position == nextPoint)
+            {
+                Debug.Log($"{pathIndex}");
+                Debug.Log($"{dna.genes.Count}");
+                nextPoint = (Vector2)transform.position + dna.genes[pathIndex];
+                pathIndex++;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, nextPoint, creatureSpeed * Time.deltaTime);
+            }
+        }
+    }
+          
+    #endregion
+
+
+
+
+}
---
+++ Genetic Algorithm - path finding/Assets/Scripts/PopulationController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationController : MonoBehaviour
+{
+
+    #region Fields
+    List<Genompathfinder> population = new List<Genompathfinder>();
+
+    public GameObject creaturePrefab;
+
+    public int populationSize = 100;
+
+    public int genomLength;
+
+    public float cutoff = 0.3f;
+
+    public Transform SpawnPoint;
+
+    public Transform End;
+    #endregion
+
+    private void Start()
+    {
+        InitPopulation();
+    }
+
+    private void Update()
+    {
+        if(!hasActive())
+        {
+            NextGeneration();
+        }
+    }
+
+    void InitPopulation()
+    {
+
+        for(int i = 0; i < populationSize;i++)
+        {
+            GameObject GO = Instantiate(creaturePrefab, SpawnPoint.position, Quaternion.identity);
+
+            GO.GetComponent<Genompathfinder>().initCreature(new DNA(genomLength = 50 ), End.position);
+
+            population.Add(GO.GetComponent<Genompathfinder>());
+        }
+    }
+
+    void NextGeneration()
+    {
+        List<Genompathfinder> survivors = new List<Genompathfinder>();
+
+        int survivalCut = Mathf.RoundToInt(populationSize*cutoff);
+
+        for(int i = 0; i < survivalCut; i ++)
+        {
+            survivors.Add(getFittest());
+        }
+
+        for(int i = 0; i < population.Count; i ++ )
+        {
+            Destroy(population[i].gameObject);
+        }
+
+        population.Clear();
+
+
+        while(population.Count < populationSize)
+        {
+            for(int i = 0; i < survivors.Count; i++)
+            {
+                GameObject GO = Instantiate(creaturePrefab, SpawnPoint.position, Quaternion.identity);
+
+                GO.GetComponent<Genompathfinder>().initCreature(new DNA(survivors[i].dna, survivors[Random.Range(0,10)].dna),End.position);
+
+                population.Add(GO.GetComponent<Genompathfinder>());
+
+                if(population.Count >= populationSize)
+                {
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < survivors.Count; i ++)
+        {
+            Destroy(survivors[i].gameObject);
+        }
+    }
+
+    Genompathfinder getFittest()
+    {
+        float maxFitness = float.MinValue;
+
+        int index = 0;
+
+        for(int i=0; i < population.Count;i++)
+        {
+            if(population[i].fitness > maxFitness)
+            {
+                maxFitness = population[i].fitness;
+                index = i;
+            }
+        }
+
+        Genompathfinder fittest = population[index];
+
+        population.Remove(fittest);
+
+        return fittest;
+    }
+
+
+    bool hasActive()
+    {
+        for(int i = 0; i < population.Count; i++)
+        {
+            if(!population[i].hasFinished)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
